07300fcd5908897d45d45af5fa5a4d6839d5617d
Wagsnerlund <chelseawagner17@gmail.com>
2021-04-03 20:21:21 +0000

    added the database schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,11 @@
 -- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+user_id SERIAL PRIMARY KEY NOT NULL,
+user_name VARCHAR(255) NOT NULL,
+user_email VARCHAR(255),
+user_password VARCHAR(255),
+user_birthday DATE,
+user_role SMALLINT,
+user_rating SMALLINT
 );
---
+++ db/schema/03_messages.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE TABLE messages (
+  msg_id SERIAL PRIMARY KEY,
+  recipient_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+  sender_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES items(item_id) ON DELETE CASCADE,
+  message TEXT,
+  msg_created TIMESTAMP
+);
