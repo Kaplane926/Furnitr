0e094365832a057e8301fbd9db125c6bb5fad503
Aimee <aimeegarriock@gmail.com>
2021-04-06 02:25:49 +0000

    routes moved to individual files

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,3 +1,15 @@
 app.get("/profile", (req, res) => {
   res.send("profile");
 });
+
+
+app.post("/profile", (req, res) => {
+  const user = user;
+  if (user) {
+    const templateVars = {
+     items: items[req.items],
+     user: users[req.users]
+    }
+    return res.render("profiles", templateVars);
+  }
+});
