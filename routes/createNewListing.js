029f2f4e7f68d108bed5b0dfee65a8a54a551f56
Eli Kaplan <elikaplan@me.com>
2021-04-08 17:57:34 +0000

    finally get the create_new_listing form working

--- routes/createNewListing.js
+++ routes/createNewListing.js
@@ -1,7 +1,7 @@
 // app.get("/new_listing", (req, res) => {
 //   res.send("new_listing");
 // });
-const moment = require("moment")
+//const moment = require("moment")
 
 const express = require('express');
 const router  = express.Router();
@@ -9,20 +9,20 @@ const router  = express.Router();
 module.exports = (db) => {
 
   router.post("/", (req, res) => {
-    console.log('afbsjd');
-    res.send(req.body)
-    const values = [req.body.title, req.body.price, req.body.description, req.body.photo, req.body.country, req.body.street, req.body.city, req.body.province, req.body.post_code]
-    const date = moment().format('l');
-   /*db.query(`
+    //console.log('afbsjd');
+
+    const values = [req.body.title, req.body.price, req.body.description, req.body.photos, req.body.country, req.body.street, req.body.city, req.body.province, req.body.post_code]
+    //const date = moment().format('l');
+   db.query(`
     INSERT INTO items (seller_id, title, price, description, image_url, date_posted, country, street, city, province, postal_code, status)
-    VALUES (1, $1, $2, $3, $4, 2020-09-09, $5, $6, $7, $8, $9, 'Available')
+    VALUES (1, $1, $2, $3, $4, '2020-09-09', $5, $6, $7, $8, $9, 'Available')
     `, values
     )
       .then(data => {
         //const users = data.rows;
         //res.json({ users });
         console.log(data)
-        res.send(data)
+        res.redirect('/my_listings')
       })
       .catch(err => {
         console.log(err)
@@ -30,7 +30,7 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
-*/
+
   });
   return router;
   };
