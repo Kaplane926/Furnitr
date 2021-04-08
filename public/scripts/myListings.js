7b7dc1078e87e64b1ed5d5120f0d24b099c24f71
Eli Kaplan <elikaplan@me.com>
2021-04-08 20:50:16 +0000

    begun updating the my listings page

---
+++ public/scripts/myListings.js
@@ -0,0 +1,49 @@
+function renderFavourites(){
+  $.ajax('/api/items/items', { method: 'POST' })
+  .then(function(data){
+    let html = ""
+    let id = 1
+    console.log(data.rows)
+    for(row of data.rows){
+      if(row.seller_id === 1){
+      html += `
+      <div class="page-title">My items</div>
+      <div class="item-card">
+        <div class="liked-items">
+        `
+      html += `<img src="${row.image_url}">
+      `
+      html += `<p class="item-title">${row.title}</p>
+      `
+      html += `<p class="item-desc">${row.description}</p>
+      `
+      html += `<button id = '${id}' class="dislike" data-id = "${row.id}">
+      <i class="fa fa-times" aria-hidden="true"></i>
+      </button>
+      </div>`
+
+    id ++
+      }
+    }
+    console.log(html)
+    $('.content').html(html)
+  })
+  .then(function(){
+    $('.dislike').click(function(){
+      const buttonId = $(this).attr('id')
+      const itemId = $(`#${buttonId}`).data('id')
+      $.ajax(`/api/unfavourite/${itemId}`, { method: 'POST' })
+      .then(function(){
+        renderFavourites()
+      })
+
+    })
+  })
+
+}
+
+$(document).ready(function(){
+
+  renderFavourites()
+
+})
